4776ee9a3b7d4e514d5ec22c1c0991e3cf165fc4
Ewelina <ekowalczyk1994@gmail.com>
2022-05-25 13:59:08 -0500

    done

--- javascript/balancing_parentheses.js
+++ javascript/balancing_parentheses.js
@@ -1,5 +1,23 @@
 function balancingParentheses(string) {
-  // type your code here
+  let opening = 0
+  let missing = 0
+  for(let i=0; i<string.length; i++){
+     if (string[i] === '('){
+         ++opening  // 1
+         continue
+     }
+
+     if(opening > 0) {
+        --opening
+     }
+     else {
+       ++missing // 3
+     }
+
+  }
+
+  return missing + opening
+
 }
 
 if (require.main === module) {
@@ -14,11 +32,37 @@ if (require.main === module) {
 
   console.log("");
 
-  console.log("Expecting: 1");
-  console.log(balancingParentheses(')'));
+  console.log("Expecting: 4");
+  console.log(balancingParentheses(')))('));
 }
 
 module.exports = balancingParentheses;
 
 // Please add your pseudocode to this file
+// we need to iterate over each element and check how many is closing and openings
+// the return value should show how many we are missing
+// let assume that missing = 0 lets assume that missing is closing
+//  if missing/closing  - openings = 0 (no missing)
+// if missing/closing - opening = 1 (missing one )
+// number should positive so i have increment missing every time
+// let opening = 0 
+// let missing = 0
+/* (for ....) {
+   if arr[0] = "("
+   increment opening  // 3 openings
+   increment missing   // 3 missings
+} 
+else {
+  if there is no ")" it means it has to be "(" so we can decrement missing
+  decrement missing  // there are 2 "(" so missing = 1
+}
+
+once loop stop running lets check
+missing = 1
+opening = 3
+
+return missing 
+
+*/
+
 // And a written explanation of your solution
